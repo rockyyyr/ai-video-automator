3e3e607abd52493760593cbbe42511af5e451eee
rockyyyr <rockyrobson@hotmail.com>
2025-06-02 15:43:09 -0700

    updated start script

--- start.bat
+++ start.bat
@@ -1,8 +1,11 @@
 @echo off
-rem — Change to your server folder and launch "npm run server" in a new window
+
 cd /d "C:\Users\aaron\brain-rot-6000\server"
 start cmd /k "npm start"
 
-rem — Change to your client folder and launch "npm run client" in a new window
 cd /d "C:\Users\aaron\brain-rot-6000\client"
 start cmd /k "npm start"
+
+timeout /t 2 /nobreak >nul
+
+start msedge "http://localhost:5174"
