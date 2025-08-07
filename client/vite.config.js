bc52a71d0318b5a9f93cbb331ff84f55bf4ced4c
rockyyyr <rockyrobson@hotmail.com>
2025-08-07 13:43:02 -0700

    updated vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,6 +7,14 @@ export default defineConfig({
     assetsInclude: ['./src/assets/fonts/*.ttf'],
     preview: {
         port: 5174,
-        allowedHosts: ['tahr-precious-lemur.ngrok-free.app', '10.0.0.109', 'localhost']
+        allowedHosts: ['tahr-precious-lemur.ngrok-free.app', '10.0.0.109', 'localhost'],
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000', // your API server
+                changeOrigin: true,
+                // if your API doesn't need the /api prefix, strip it:
+                rewrite: (p) => p.replace(/^\/api/, ''),
+            }
+        }
     },
 });
